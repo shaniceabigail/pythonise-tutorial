4fe1d848b572fc4de230a85ff0bdd6a3067edcb1
Shanice Abigail <shanice.abigail@hpe.com>
2022-03-23 10:14:11 +0800

    ep 5 - jinja template inheritance

--- app/admin_views.py
+++ app/admin_views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route("/admin/dashboard")
 def admin_dashboard():
     return render_template("admin/dashboard.html")
+
+@app.route("/admin/profile")
+def admin_profile():
+    return render_template("admin/profile.html")
--- app/views.py
+++ app/views.py
@@ -7,8 +7,4 @@ def index():
 
 @app.route("/about")
 def about():
-    return """
-    <h1 style='color: red;'>I'm a red H1 heading!</h1>
-    <p>This is a lovely little paragraph</p>
-    <code>Flask is <em>awesome</em></code>
-    """
+    return render_template("public/about.html")

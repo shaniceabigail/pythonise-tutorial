6634c2520ae75caf658800d3bcc10a10357557be
Shanice Abigail <shanice.abigail@hpe.com>
2022-03-24 17:37:33 +0800

    ep 7 - form validation

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from tkinter import N
 from turtle import color
 from app import app 
-from flask import render_template
+from flask import render_template, request, redirect
 from datetime import datetime
 
 @app.route("/")
@@ -11,6 +11,36 @@ def index():
 @app.route("/about")
 def about():
     return render_template("public/about.html")
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        req = request.form
+        missing = list()
+
+        for k, v in req.items():
+            if v == "":
+                missing.append(k)
+
+        if missing:
+            feedback = f"Missing fields for {', '.join(missing)}"
+            return render_template("public/sign_up.html", feedback=feedback)
+        # username = req.get("username")
+        # email = req["email"]
+        # password = request.form["password"]
+
+        # password = request.form.get("password")
+
+        return redirect(request.url)
+
+
+
+    return render_template("public/sign_up.html")
+
+
+
+
+
 
 @app.route("/jinja")
 def jinja():

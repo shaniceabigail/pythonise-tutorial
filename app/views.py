899d3f0f1066cd23db5e7924d6115dc208bfef35
Shanice Abigail <shanice.abigail@hpe.com>
2022-03-25 09:44:00 +0800

    ep 10 - fetch setup

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import json
 import re
 from tkinter import N
 from turtle import color
-from app import app 
+from app import app
 from flask import render_template, request, redirect, jsonify, make_response
 from datetime import datetime
 
@@ -25,13 +25,16 @@ users = {
     }
 }
 
+
 @app.route("/")
 def index():
     return render_template("public/index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("public/about.html")
+
 
 @app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
@@ -54,8 +57,6 @@ def sign_up():
 
         return redirect(request.url)
 
-
-
     return render_template("public/sign_up.html")
 
 
@@ -67,6 +68,7 @@ def profile(username):
 
     return render_template("public/profile.html", username=username, user=user)
 
+
 @app.route("/multiple/<foo>/<bar>/<baz>")
 def multiple(foo, bar, baz):
     print(f"foo is {foo}")
@@ -75,8 +77,25 @@ def multiple(foo, bar, baz):
 
     return f"foo is {foo}, bar is {bar}, baz is {baz}"
 
+# Guestbook
 
 
+@app.route("/guestbook")
+def guestbook():
+    return render_template("public/guestbook.html")
+
+
+@app.route("/guestbook/create-entry", methods=["POST"])
+def create_entry():
+    req = request.get_json()
+    print(req)
+
+    res = make_response(jsonify({"message": "OK"}), 200)
+
+    return res
+
+
+# JSON example
 @app.route("/json", methods=["POST"])
 def json_example():
 
@@ -92,20 +111,20 @@ def json_example():
         res = make_response(jsonify(response_body), 200)
 
         return res
-    
+
     else:
         return make_response(jsonify({"message": "Request body must be JSON"}), 400)
 
 
-
+# Jinja examples
 @app.route("/jinja")
 def jinja():
-    #Strings
+    # Strings
     my_name = "Julian"
     my_html = "<h1>This is some HTML</h1>"
     suspicious = "<script>alert('NEVER TRUST USER INPUT!')</script>"
 
-    #Integers
+    # Integers
     my_age = 30
 
     # Lists
@@ -138,7 +157,7 @@ def jinja():
 
         def clone(self, repo):
             return f"Cloning into {repo}"
-    
+
     my_remote = GitRemote(
         name="Learning Flask",
         description="Learn the Flask Web Framework for Python",
@@ -149,14 +168,13 @@ def jinja():
     def repeat(x, qty=1):
         return x * qty
 
-
     date = datetime.utcnow()
 
     return render_template("public/jinja.html", my_name=my_name, my_age=my_age, langs=langs,
-        friends=friends, colors=colors, cool=cool, GitRemote=GitRemote, my_remote=my_remote, 
-        repeat=repeat, date=date, my_html=my_html, suspicious=suspicious)
+                           friends=friends, colors=colors, cool=cool, GitRemote=GitRemote, my_remote=my_remote,
+                           repeat=repeat, date=date, my_html=my_html, suspicious=suspicious)
 
 
-@app.template_filter("clean_date")    
+@app.template_filter("clean_date")
 def clean_date(dt):
     return dt.strftime("%d %b %Y")
